c468e28b39ca3917c5765382e7a7cbd92c9d435c
Jochen Diepelt <David.diepelt@gmx.net>
2025-10-31 14:18:47 +0100

    making things reproducible

--- source/buildall.bash
+++ source/buildall.bash
@@ -3,6 +3,9 @@
 set -euo pipefail
 IFS=$'\n\t' # Strict Internal Field Separator
 
+SOURCE_TIME_EPOCH=$(git log -1 --pretty="format:%ct")
+export SOURCE_TIME_EPOCH
+
 # Generate all files from the .ins file to create classes and packages
 latex tudcd.ins
 
@@ -10,6 +13,7 @@ latex tudcd.ins
 for file in *; do
   fileextension=${file##*.}
   if [[ -f $file && ($fileextension == "dtx") ]]; then
-    pdflatex -interaction=nonstopmode $file
+    status=$(pdflatex -interaction=nonstopmode -draftmode $file)
+    echo "Status $status"
   fi
 done
