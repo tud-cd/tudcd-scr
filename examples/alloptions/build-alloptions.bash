9e5344a0d82105ed1ccf991eade345f8f1d0f2f4
Jochen Diepelt <David.diepelt@gmx.net>
2025-12-10 15:54:31 +0100

    aktualisieren des optionenkatalogs

--- examples/alloptions/build-alloptions.bash
+++ examples/alloptions/build-alloptions.bash
@@ -7,11 +7,13 @@ if [[ -f "out.pdf" ]]; then
   rm out.pdf
 fi
 
-
+# Erstellen der Zusammenfassung
+echo " " > summary.tex
 for mainfile in main*.tex; do
     #echo $mainfile
     class=${mainfile%.*}
     class=${class##main-}
+    echo "\\section{$class}" >> summary.tex
     for logocolor in logocolor-*; do
         #echo $logocolor
         color=${logocolor%.*}
@@ -25,17 +27,28 @@ for mainfile in main*.tex; do
                 margin=${usemargin%.*}
                 margin=${margin##margin-}
 
-                echo "\\subtitle{Mit $color, $lang, $margin}" > tempsubtitle.tex
+                echo "\\subtitle{Mit Optionen Logofarbe $color, Logosprache $lang und Marginalie $margin}" > tempsubtitle.log
 
-                cat $mainfile $logocolor $logolang $usemargin tempsubtitle.tex "content-$class.tex" > tempmain.tex
+                cat $mainfile $logocolor $logolang $usemargin tempsubtitle.log "content-$class.tex" > tempmain.tex
 
                 pdflatex "-jobname=main-$class-$color-$lang-$margin" tempmain.tex
+                pdflatex "-jobname=main-$class-$color-$lang-$margin" tempmain.tex
+
+                pages=$(qpdf main-$class-$color-$lang-$margin.pdf --show-npages)
+
+                echo "\\includepdf[pages=-,link=true,addtotoc={1,subsection,2,$class $color $lang $margin,subsec:$class$color$lang$margin}]{main-$class-$color-$lang-$margin.pdf}" >> summary.tex
             done
         done
     done
 done
 
-qpdf --empty --pages *.pdf -- out.pdf
+pdflatex -jobname=out pdfpagecollector.tex
+pdflatex -jobname=out pdfpagecollector.tex
+pdflatex -jobname=out pdfpagecollector.tex
+
+rm summary.tex
+rm tempmain.tex
+#qpdf --empty --pages *.pdf -- out.pdf
 mv out.pdf ../out.pdf
 rm *.pdf
 rm *.log
